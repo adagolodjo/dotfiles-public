5bf0ca508b3a13496c2ac000c2de893e5cd949ec
Yinoussa Adagolodjo <yinoussa.adagolodjo2@univ-lille.fr>
2023-10-16 17:45:21 +0200

    update .config/nvim/initi.lua to take into account the folder

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,11 @@
 if vim.loader then
 	vim.loader.enable()
 end
+require("craftzdog.base")
+require("craftzdog.highlights")
+require("craftzdog.maps")
+require("craftzdog.plugins")
+require("lua.plugins")
 
 _G.dd = function(...)
 	require("util.debug").dump(...)
